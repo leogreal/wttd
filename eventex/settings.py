33ae38a82cb6f2f355ce83371886682f5c0749b2
Leonardo <leogreal@gmail.com>
2016-03-10 09:31:41 -0300

    Correcao do nome do modulo

--- eventex/settings.py
+++ eventex/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'test_without_migrations',
-    'django_extensions',
+    'django-extensions',
     'eventex.core',
     'eventex.subscriptions',
 ]
