22106e189e9ef506ff5f0ccab7487cdc43f0ca2a
Leonardo Gregório de Almeida <leogreal@gmail.com>
2017-11-19 20:02:11 -0200

    Padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -20,6 +20,11 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_captalized(self):
+        """Name must be captalized"""
+        form = self.make_validated_form(name='LEONARDO gregorio de ALmeiDA')
+        self.assertEqual('Leonardo Gregorio De Almeida', form.cleaned_data['name'])
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
