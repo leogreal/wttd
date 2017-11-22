b372d94202997bde73bca3c707de4f88628f025f
Leonardo Gregório de Almeida <leogreal@gmail.com>
2017-11-22 19:24:47 -0200

    Simplifica com modelform

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -82,11 +82,11 @@ class SubscriptionsNewPostInvalid(TestCase):
 
 
 class TemplateRegressionTest(TestCase):
-    def test_template_has_non_field_error(self):
+    def test_template_has_non_field_errors(self):
         invalid_data = dict(
             name='Henrique Bastos',
             cpf='12345678901'
         )
         response = self.client.post(r('subscriptions:new'), invalid_data)
 
-        self.assertContains(response, '<ul class="errorlist nonfield>"')
+        self.assertContains(response, '<ul class="errorlist nonfield">')
