e3edb0da1359c7826fed05edf78bddaa7196eecd
Leonardo Gregório de Almeida <leogreal@gmail.com>
2018-01-26 21:47:23 -0200

    remove import not used

--- eventex/settings.py
+++ eventex/settings.py
@@ -15,7 +15,6 @@ from decouple import config, Csv
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from django.template.defaultfilters import default
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
